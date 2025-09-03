9489a010dd26d7440e75e3991ab1b7d97859d51c
Jimmy <46695172+RalphJnr@users.noreply.github.com>
2025-09-03 22:34:41 +0100

    update

---
+++ index.js
@@ -0,0 +1,33 @@
+const homePageContainer = document.getElementById("homepage");
+const postPageContainer = document.getElementById("post-page");
+const aboutUsContainer = document.getElementById("about-us");
+const link = document.querySelector("a");
+
+// postLink;
+// aboutLink;
+
+// link.addEventListener("click", function (e) {
+//   e.preventDefault();
+// });
+
+document.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.id === "postLink") {
+    homePageContainer.classList.add("hide");
+    aboutUsContainer.classList.add("hide");
+    postPageContainer.classList.remove("hide");
+  }
+
+  if (e.target.id === "aboutLink") {
+    homePageContainer.classList.add("hide");
+    aboutUsContainer.classList.remove("hide");
+    postPageContainer.classList.add("hide");
+  }
+
+  if (e.target.id === "home") {
+    homePageContainer.classList.remove("hide");
+    aboutUsContainer.classList.add("hide");
+    postPageContainer.classList.add("hide");
+  }
+});
